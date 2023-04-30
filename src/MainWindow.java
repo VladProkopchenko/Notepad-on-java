71388ad36631246070fe8fde1377ce7d8bac1603
Vlad <prokopchenko@mail.ru>
2023-04-30 19:37:43 +0300

    final cut

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,10 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 
 public class MainWindow extends JFrame {
+
+    private final JTextArea editField;
+    private final JComboBox fileSettings;
+    private final JComboBox fontStyles;
+    private final JSpinner fontSizeSpinner;
+    private  JButton colorButton;
+    JPanel head;
     MainWindow(){
         super("Editor");
 
@@ -23,81 +28,93 @@ public class MainWindow extends JFrame {
         };
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        JTextArea editField = new JTextArea(32,77);
-        editField.setLineWrap (true);
-        editField.setWrapStyleWord (false);
-        JComboBox fileSettings = new JComboBox(settings);
-        JComboBox fontStyles = new JComboBox(styles);
-        fileSettings.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    if (fileSettings.getSelectedIndex() == 1) {
-                        BufferedWriter writer = new BufferedWriter(new FileWriter("file.txt",false));
-                        editField.write(writer);
-                        fileSettings.setSelectedIndex(0);
-                    }
-                     if(fileSettings.getSelectedIndex()==2){
-                         JFileChooser fileSaver = new JFileChooser();
-                         int ret = fileSaver.showDialog(null, "Save file");
-                         if (ret == JFileChooser.APPROVE_OPTION) {
-                             File file = fileSaver.getSelectedFile();
-                             BufferedWriter writer = new BufferedWriter(new FileWriter(file,false));
-                             editField.write(writer);
-                             fileSettings.setSelectedIndex(0);
-                         }
-
-                     }
-                     if(fileSettings.getSelectedIndex()==3){
-                         JFileChooser fileLoader = new JFileChooser();
-                         int ret = fileLoader.showDialog(null,"Load file");
-                         if(ret == JFileChooser.APPROVE_OPTION){
-                             File file = fileLoader.getSelectedFile();
-                             BufferedReader reader = new BufferedReader(new FileReader(file));
-                             String textLine;
-                             StringBuilder text = new StringBuilder();
-                             while((textLine=reader.readLine())!=null){
-                                 text.append(textLine);
-                                 text.append("\n");
-                             }
-                             editField.setText(text.toString());
-                         }
-                     }
-                     if(fileSettings.getSelectedIndex()==4){
-                         System.exit(0);
-                     }
-                    }catch (IOException ex){
-                    JOptionPane.showMessageDialog(null, "Something was wrong !&#7?");
+        editField = new JTextArea();
+        editField.setLineWrap(true);
+        fileSettings = new JComboBox(settings);
+        fontStyles = new JComboBox(styles);
+        colorButton = new JButton("Choose text color");
+        SpinnerModel model = new SpinnerNumberModel(35,15,99,1);
+        fontSizeSpinner = new JSpinner(model);
+        fontSizeSpinner.setSize(50, 80);
+        editField.setFont(new Font("...",Font.PLAIN,35));
+        JScrollPane scrollPane = new JScrollPane(editField);
+        fileSettings.addActionListener(e -> {
+            try {
+                if (fileSettings.getSelectedIndex() == 1) {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("file.txt",false));
+                    editField.write(writer);
+                    fileSettings.setSelectedIndex(0);
                 }
+                 if(fileSettings.getSelectedIndex()==2){
+                     JFileChooser fileSaver = new JFileChooser();
+                     int ret = fileSaver.showDialog(null, "Save file");
+                     if (ret == JFileChooser.APPROVE_OPTION) {
+                         File file = fileSaver.getSelectedFile();
+                         BufferedWriter writer = new BufferedWriter(new FileWriter(file,false));
+                         editField.write(writer);
+                         fileSettings.setSelectedIndex(0);
+                     }
+
+                 }
+                 if(fileSettings.getSelectedIndex()==3){
+                     JFileChooser fileLoader = new JFileChooser();
+                     int ret = fileLoader.showDialog(null,"Load file");
+                     if(ret == JFileChooser.APPROVE_OPTION){
+                         File file = fileLoader.getSelectedFile();
+                         BufferedReader reader = new BufferedReader(new FileReader(file));
+                         String textLine;
+                         StringBuilder text = new StringBuilder();
+                         while((textLine=reader.readLine())!=null){
+                             text.append(textLine);
+                             text.append("\n");
+                         }
+                         editField.setText(text.toString());
+                     }
+                 }
+                 if(fileSettings.getSelectedIndex()==4){
+                     System.exit(0);
+                 }
+                }catch (IOException ex){
+                JOptionPane.showMessageDialog(null, "Something was wrong !&#7?");
             }
         });
-        fontStyles.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(fontStyles.getSelectedIndex()==1){
-                    editField.setFont(new Font("...", Font.BOLD, 12));
-                    fontStyles.setSelectedIndex(0);
-                }
-                if(fontStyles.getSelectedIndex()==2){
-                    editField.setFont(new Font("...", Font.PLAIN, 12));
-                    fontStyles.setSelectedIndex(0);
-                }
-                if(fontStyles.getSelectedIndex()==3){
-                    editField.setFont(new Font("...", Font.ITALIC, 12));
-                    fontStyles.setSelectedIndex(0);
-                }
+        fontStyles.addActionListener(e -> {
+            Font font = editField.getFont();
+            if(fontStyles.getSelectedIndex()==1){
+                editField.setFont(new Font("...",Font.BOLD,font.getSize()));
+                fontStyles.setSelectedIndex(0);
             }
+            if(fontStyles.getSelectedIndex()==2){
+                editField.setFont(new Font("...",Font.PLAIN,font.getSize()));
+                fontStyles.setSelectedIndex(0);
+            }
+            if(fontStyles.getSelectedIndex()==3){
+                editField.setFont(new Font("...",Font.ITALIC,font.getSize()));
+                fontStyles.setSelectedIndex(0);
+            }
+        });
+
+        fontSizeSpinner.addChangeListener(e -> {
+            Font font = editField.getFont();
+            int size = (Integer) fontSizeSpinner.getValue();
+            editField.setFont(new Font("...",font.getStyle(),size));
+        });
+
+        colorButton.addActionListener(e ->{
+            Color color = JColorChooser.showDialog(null,"Choose the color",Color.BLACK);
+            Font font = editField.getFont();
+            editField.setForeground(color);
         });
 
         Container area = getContentPane();
-        JPanel head = new JPanel();
-
+        head = new JPanel();
         head.setLayout(new FlowLayout());
         head.add(fileSettings);
         head.add(fontStyles);
-        area.setLayout(new FlowLayout());
-        area.add(head);
-        area.add(editField);
+        head.add(fontSizeSpinner);
+        head.add(colorButton);
+        area.add(head,BorderLayout.NORTH);
+        area.add(scrollPane,BorderLayout.CENTER);
         setSize(800,600);
         setLocation(300,100);
     }
